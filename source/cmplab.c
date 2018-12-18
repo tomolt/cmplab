96a6cbbbb2609970ab8e5692f5921bfb66fe7ca0
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2018-12-18 13:49:04 +0100

    [untested] crude lzw encoder.

--- source/cmplab.c
+++ source/cmplab.c
@@ -1,8 +1,47 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct {
+	int prefix; // index into dict
+	int suffix; // a symbol
+} lzw_word;
+
+void encode_lzw(FILE *in, FILE *out)
+{
+	lzw_word dict[65536];
+	for (int i = 0; i < 256; ++i)
+		dict[i] = (lzw_word){-1, i};
+	int top = 256;
+	int sym = fgetc(in);
+	if (sym == EOF) return;
+	int index = sym;
+	for (;;) {
+		sym = fgetc(in);
+		if (sym == EOF) break;
+		lzw_word word = {index, sym};
+		int succ = -1;
+		for (int i = 0; i < top; ++i) {
+			if (dict[i].prefix != word.prefix) continue;
+			if (dict[i].suffix != word.suffix) continue;
+			succ = i;
+			break;
+		}
+		if (succ >= 0) {
+			index = succ;
+		} else {
+			dict[top++] = word;
+			fputc(index / 256, out);
+			fputc(index % 256, out);
+			index = sym;
+		}
+	}
+	fputc(index / 256, out);
+	fputc(index % 256, out);
+}
+
 int main(int argc, char *argv[])
 {
 	(void) argc, (void) argv;
+	encode_lzw(stdin, stdout);
 	return EXIT_SUCCESS;
 }
