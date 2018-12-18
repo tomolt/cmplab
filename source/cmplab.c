fabc18448d64b237562ac69fc9e9c92d5390250c
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2018-12-18 21:49:47 +0100

    Huffman coding is in progress.

--- source/cmplab.c
+++ source/cmplab.c
@@ -24,6 +24,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <assert.h>
 
 typedef struct {
 	int prefix; // index into dict
@@ -126,15 +128,110 @@ void decode_lzw(FILE *in, FILE *out)
 	}
 }
 
+static void countfreqs(FILE *in, int freqs[256])
+{
+	for (int i = 0; i < 256; ++i)
+		freqs[i] = 0;
+	for (;;) {
+		int sym = fgetc(in);
+		if (sym == EOF) break;
+		++freqs[sym];
+	}
+}
+
+typedef struct {
+	int *freqs;
+	int *heap;
+	int count;
+} hf_queue;
+
+static void percup(hf_queue q, int at, int sym)
+{
+	int const p = (at - 1) / 2;
+	if (at > 0 && q.freqs[sym] < q.freqs[q.heap[p]]) {
+		q.heap[at] = q.heap[p];
+		percup(q, p, sym);
+	} else {
+		q.heap[at] = sym;
+	}
+}
+
+static void percdown(hf_queue q, int at, int sym)
+{
+	int const left  = 2 * at + 1;
+	int const right = 2 * at + 2;
+	if (left < q.count && q.freqs[sym] > q.freqs[q.heap[left]]) {
+		q.heap[at] = q.heap[left];
+		percdown(q, left, sym);
+	} else if (right < q.count && q.freqs[sym] > q.freqs[q.heap[right]]) {
+		q.heap[at] = q.heap[right];
+		percdown(q, right, sym);
+	} else {
+		q.heap[at] = sym;
+	}
+}
+
+static void freq2hier(int freqs[256], int hier[511])
+{
+	for (int i = 0; i < 511; ++i)
+		hier[i] = -1;
+	int heap[256];
+	hf_queue q = {freqs, heap, 0};
+	for (int sym = 0; sym < 256; ++sym) {
+		if (freqs[sym] == 0) continue;
+		q.heap[q.count++] = sym;
+	}
+	for (int i = 127; i >= 0; --i)
+		percdown(q, i, q.heap[i]);
+	int next_compound = 256;
+	while (q.count > 1) {
+		int first  = q.heap[0];
+		percdown(q, 0, q.heap[--q.count]);
+		int second = q.heap[0];
+		percdown(q, 0, q.heap[--q.count]);
+		int new = next_compound++;
+		hier[first] = new;
+		hier[second] = new;
+		percup(q, q.count++, new);
+	}
+}
+
+#if 0
+static void hier2len(int hier[511], int len[256])
+{
+	lengths[510] = 1;
+	for (int i = 509; i >= 0; --i) {
+		lengths[i] = lengths[parents[i]];
+	}
+}
+#endif
+
 int main(int argc, char *argv[])
 {
 	(void) argc, (void) argv;
 
+	int freqs[256];
+	countfreqs(stdin, freqs);
+
+	int hier[511];
+	freq2hier(freqs, hier);
+
+	for (int i = 0; i < 511; ++i) {
+		if (hier[i] < 0) continue;
+		if (i > 32 && i < 127) {
+			fprintf(stdout, "'%c' -> %03d\n", i, hier[i]);
+		} else {
+			fprintf(stdout, "%03d -> %03d\n", i, hier[i]);
+		}
+	}
+
+#if 0
 	FILE *buf = tmpfile();
 	encode_lzw(stdin, buf);
 	rewind(buf);
 	decode_lzw(buf, stdout);
 	fclose(buf);
+#endif
 
 	return EXIT_SUCCESS;
 }
