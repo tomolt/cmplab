745c97f97a44628e0fac50f435090c81012bdebb
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2018-12-22 16:28:58 +0100

    In the process of separating integer types for representing symbols /indices etc. for clarity and future expansion.

--- main/main.c
+++ main/main.c
@@ -30,21 +30,31 @@
 
 #include "band.h"
 
+typedef int32_t Symbol;
+typedef Symbol  Synsym; // synthetic symbol
+typedef int64_t Count; // roughly proportional to the length of the input
+
+#define ALPHABET_SIZE 256
+
+#define LZW_DICT_SIZE 65536
+
+typedef LzwIdx;
+
 typedef struct {
-	int prefix; // index into dict
-	int suffix; // a symbol
+	LzwIdx prefix;
+	Symbol suffix;
 } lzw_word;
 
-static int initdict(lzw_word dict[65536])
+static LzwIdx initdict(lzw_word dict[LZW_DICT_SIZE])
 {
-	for (int i = 0; i < 256; ++i)
-		dict[i] = (lzw_word){-1, i};
-	return 256;
+	for (Symbol sym = 0; sym < ALPHABET_SIZE; ++sym)
+		dict[sym] = (lzw_word){-1, sym};
+	return ALPHABET_SIZE;
 }
 
-static int findword(lzw_word dict[65536], int top, int index, int sym)
+static LzwIdx findword(lzw_word dict[LZW_DICT_SIZE], LzwIdx top, LzwIdx index, Symbol sym)
 {
-	for (int i = 0; i < top; ++i) {
+	for (LzwIdx i = 0; i < top; ++i) {
 		if (dict[i].prefix != index) continue;
 		if (dict[i].suffix != sym) continue;
 		return i;
@@ -54,19 +64,20 @@ static int findword(lzw_word dict[65536], int top, int index, int sym)
 
 void encode_lzw(FILE *in, Band *out)
 {
-	lzw_word dict[65536];
-	int top = initdict(dict);
-	int bitsize = 9;
+	lzw_word dict[LZW_DICT_SIZE];
+	LzwIdx top = initdict(dict);
 
-	int sym = fgetc(in);
+	int bitsize = 1;
+	while (ALPHABET_SIZE >> bitsize > 0) ++bitsize;
+
+	LzwIdx index = fgetc(in);
 	if (feof(in)) return;
-	int index = sym;
 
 	for (;;) {
-		sym = fgetc(in);
+		Symbol sym = fgetc(in);
 		if (feof(in)) break;
 
-		int succ = findword(dict, top, index, sym);
+		LzwIdx succ = findword(dict, top, index, sym);
 
 		if (succ >= 0) {
 			index = succ;
@@ -84,7 +95,7 @@ void encode_lzw(FILE *in, Band *out)
 	bwritebits(out, bitsize, index);
 }
 
-static int firstsym(lzw_word dict[65536], int idx)
+static Symbol firstsym(lzw_word dict[LZW_DICT_SIZE], LzwIdx idx)
 {
 	if (dict[idx].prefix < 0)
 		return dict[idx].suffix;
@@ -92,7 +103,7 @@ static int firstsym(lzw_word dict[65536], int idx)
 		return firstsym(dict, dict[idx].prefix);
 }
 
-static void fputword(lzw_word dict[65536], int idx, FILE *out)
+static void fputword(lzw_word dict[LZW_DICT_SIZE], LzwIdx idx, FILE *out)
 {
 	if (idx < 0) return;
 	fputword(dict, dict[idx].prefix, out);
@@ -101,19 +112,21 @@ static void fputword(lzw_word dict[65536], int idx, FILE *out)
 
 void decode_lzw(Band *in, FILE *out)
 {
-	lzw_word dict[65536];
-	int top = initdict(dict);
-	int bitsize = 9;
+	lzw_word dict[LZW_DICT_SIZE];
+	LzwIdx top = initdict(dict);
 
-	int index = breadbits(in, bitsize);
+	int bitsize = 1;
+	while (ALPHABET_SIZE >> bitsize > 0) ++bitsize;
+
+	LzwIdx index = breadbits(in, bitsize);
 	if (feof(in->file)) return;
 	fputword(dict, index, out);
 
 	for (;;) {
-		int succ = breadbits(in, bitsize);
+		LzwIdx succ = breadbits(in, bitsize);
 		if (feof(in->file)) break;
 
-		int sym = firstsym(dict, succ < top ? succ : index);
+		Symbol sym = firstsym(dict, succ < top ? succ : index);
 		dict[top++] = (lzw_word) {index, sym};
 
 		if (top >= (1 << bitsize) - 2) {
@@ -126,26 +139,26 @@ void decode_lzw(Band *in, FILE *out)
 	}
 }
 
-static void countfreqs(FILE *in, int freqs[256])
+static void countfreqs(FILE *in, Count freqs[ALPHABET_SIZE])
 {
-	for (int i = 0; i < 256; ++i)
-		freqs[i] = 0;
+	for (Symbol sym = 0; sym < ALPHABET_SIZE; ++sym)
+		freqs[sym] = 0;
 	for (;;) {
-		int sym = fgetc(in);
+		Symbol sym = fgetc(in);
 		if (feof(in)) break;
 		++freqs[sym];
 	}
 }
 
 typedef struct {
-	int *freqs;
-	int *heap;
-	int count;
+	Count *freqs;
+	Synsym *heap;
+	Count count;
 } hf_queue;
 
-static void percup(hf_queue q, int at, int sym)
+static void percup(hf_queue q, Count at, Synsym sym)
 {
-	int const p = (at - 1) / 2;
+	Count const p = (at - 1) / 2;
 	if (at > 0 && q.freqs[sym] < q.freqs[q.heap[p]]) {
 		q.heap[at] = q.heap[p];
 		percup(q, p, sym);
@@ -154,10 +167,10 @@ static void percup(hf_queue q, int at, int sym)
 	}
 }
 
-static void percdown(hf_queue q, int at, int sym)
+static void percdown(hf_queue q, Count at, Synsym sym)
 {
-	int const left  = 2 * at + 1;
-	int const right = 2 * at + 2;
+	Count const left  = 2 * at + 1;
+	Count const right = 2 * at + 2;
 	if (left < q.count && q.freqs[sym] > q.freqs[q.heap[left]]) {
 		q.heap[at] = q.heap[left];
 		percdown(q, left, sym);
@@ -169,29 +182,29 @@ static void percdown(hf_queue q, int at, int sym)
 	}
 }
 
-static int freq2hier(int sfreqs[256], int hier[511])
+static Count freq2hier(Count sfreqs[ALPHABET_SIZE], Symbol hier[ALPHABET_SIZE * 2 - 1])
 {
-	int heap[256];
-	int freqs[511];
-	for (int i = 0; i < 256; ++i)
+	Synsym heap[ALPHABET_SIZE];
+	Count freqs[ALPHABET_SIZE * 2 - 1];
+	for (Symbol i = 0; i < ALPHABET_SIZE; ++i)
 		freqs[i] = sfreqs[i];
 	hf_queue q = {freqs, heap, 0};
-	for (int sym = 0; sym < 256; ++sym) {
+	for (Symbol sym = 0; sym < ALPHABET_SIZE; ++sym) {
 		if (freqs[sym] == 0) continue;
 		q.heap[q.count++] = sym;
 	}
-	for (int i = q.count / 2 - 1; i >= 0; --i)
+	for (Count i = q.count / 2 - 1; i >= 0; --i)
 		percdown(q, i, q.heap[i]);
 
-	for (int i = 0; i < 511; ++i)
+	for (Synsym i = 0; i < ALPHABET_SIZE * 2 - 1; ++i)
 		hier[i] = -1;
-	int ncount = 256; // node count
+	Count ncount = ALPHABET_SIZE; // node count
 	while (q.count > 1) {
-		int first  = q.heap[0];
+		Synsym first  = q.heap[0];
 		percdown(q, 0, q.heap[--q.count]);
-		int second = q.heap[0];
+		Synsym second = q.heap[0];
 		percdown(q, 0, q.heap[--q.count]);
-		int new = ncount++;
+		Synsym new = ncount++;
 		q.freqs[new] = q.freqs[first] + q.freqs[second];
 		hier[first] = new;
 		hier[second] = new;
@@ -200,9 +213,9 @@ static int freq2hier(int sfreqs[256], int hier[511])
 	return ncount;
 }
 
-static void hier2len(int hier[511], int ncount, int len[256])
+static void hier2len(Synsym hier[ALPHABET_SIZE * 2 - 1], Count ncount, int len[ALPHABET_SIZE])
 {
-	int depth[511];
+	int depth[ALPHABET_SIZE * 2 - 1];
 	depth[ncount - 1] = 1;
 	for (int i = ncount - 2; i >= 0; --i) {
 		if (hier[i] < 0) {
@@ -211,17 +224,17 @@ static void hier2len(int hier[511], int ncount, int len[256])
 			depth[i] = depth[hier[i]] + 1;
 		}
 	}
-	for (int i = ncount - 1; i < 256; ++i)
+	for (int i = ncount - 1; i < ALPHABET_SIZE; ++i)
 		depth[i] = -1;
-	for (int i = 0; i < 256; ++i)
+	for (int i = 0; i < ALPHABET_SIZE; ++i)
 		len[i] = depth[i];
 }
 
 static int symlen_compare(void const *ap, void const *bp, void *ud)
 {
 	int *len = ud;
-	int as = * (int *) ap;
-	int bs = * (int *) bp;
+	Symbol as = * (Symbol *) ap;
+	Symbol bs = * (Symbol *) bp;
 	int ld = len[as] - len[bs];
 	if (ld != 0) {
 		return ld;
@@ -230,23 +243,23 @@ static int symlen_compare(void const *ap, void const *bp, void *ud)
 	}
 }
 
-static void symsbylen(int len[256], int syms[256])
+static void symsbylen(int len[ALPHABET_SIZE], Symbol syms[ALPHABET_SIZE])
 {
 	// TODO filter out unused symbols before sorting
-	for (int i = 0; i < 256; ++i)
-		syms[i] = i;
-	qsort_r(syms, 256, sizeof(*syms), symlen_compare, len);
+	for (Symbol sym = 0; sym < ALPHABET_SIZE; ++sym)
+		syms[sym] = sym;
+	qsort_r(syms, ALPHABET_SIZE, sizeof(*syms), symlen_compare, len);
 }
 
-static void len2code(int syms[256], int len[256], unsigned long code[256])
+static void len2code(int syms[ALPHABET_SIZE], int len[ALPHABET_SIZE], unsigned long code[ALPHABET_SIZE])
 {
 	int sym_start = 0;
 	while (len[syms[sym_start]] < 0) ++sym_start;
 
 	unsigned long next = 0;
 	int prev_len = len[syms[sym_start]];
-	for (int i = sym_start; i < 256; ++i) {
-		int sym = syms[i];
+	for (int i = sym_start; i < ALPHABET_SIZE; ++i) {
+		Symbol sym = syms[i];
 		next <<= len[sym] - prev_len;
 		code[sym] = next++;
 		prev_len = len[sym];
@@ -255,7 +268,7 @@ static void len2code(int syms[256], int len[256], unsigned long code[256])
 
 void encode_huff(FILE *in, Band *out, Band *table)
 {
-	int freqs[256], hier[511], len[256], syms[256];
+	int freqs[ALPHABET_SIZE], hier[ALPHABET_SIZE * 2 - 1], len[ALPHABET_SIZE], syms[ALPHABET_SIZE];
 	countfreqs(stdin, freqs);
 	int ncount = freq2hier(freqs, hier);
 	hier2len(hier, ncount, len);
@@ -264,10 +277,10 @@ void encode_huff(FILE *in, Band *out, Band *table)
 
 	rewind(in);
 	symsbylen(len, syms);
-	unsigned long code[256];
+	unsigned long code[ALPHABET_SIZE];
 	len2code(syms, len, code);
 	for (;;) {
-		int sym = fgetc(in);
+		Symbol sym = fgetc(in);
 		if (feof(in)) break;
 
 		bwritebits(out, len[sym], code[sym]);
